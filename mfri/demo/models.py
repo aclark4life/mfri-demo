ea8298f31731b0d7ef9bad8ab94dbd0329892c23
Alex Clark <aclark@aclark.net>
2017-08-18 07:40:33 -0400

    Update

--- mfri/demo/models.py
+++ mfri/demo/models.py
@@ -4,3 +4,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+# http://docs.wagtail.io/en/v1.11.1/getting_started/tutorial.html#extend-the-homepage-model
+
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+
+
+class HomePage(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
